4aa7ebfed45ae3ae49c7273c337067046bc4f204
Fiodor Furick <fff24.06.91@gmail.com>
2021-01-05 12:23:47 +0200

    add styles css

---
+++ src/components/todo-list.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import TodoListItem from "./todo-list-item";
+
+import './todo-list.css';
+
+const TodoList = ({todos}) => {
+
+    const todoEl = todos.map(
+        (item) => {
+            const {id, ...itemProps} = item;
+            return (
+                <li key={id} className={'list-group-item'}>
+                    <TodoListItem {...itemProps}/>
+                </li>
+            )
+        });
+
+    return (
+        <ul className={'list-group todo-list'}>
+            {todoEl}
+        </ul>
+    );
+};
+
+export default TodoList;
+
