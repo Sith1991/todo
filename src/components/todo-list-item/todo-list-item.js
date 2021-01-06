d670d7f488a17e52b66b371016aec011d8d20ad0
Fiodor Furick <fff24.06.91@gmail.com>
2021-01-07 01:37:27 +0200

    add state to todo-list-item.js

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -4,29 +4,42 @@ import './todo-list-item.css';
 
 export default class TodoListItem extends Component {
 
+    state = {
+        done: false,
+    }
+
     onClickLabel = () => {
-        console.log(`Done: ${this.props.label}`);
+        this.setState({
+            done: true
+        })
     }
 
-    render () {
- const {label, important = false} = this.props;
+    render() {
+        const {label, important = false} = this.props;
 
-    const style = {
-        color: important ? 'tomato' : 'black',
-        fontWeight: important ? 'bold' : 'normal'
-    }
-    return (
-        <span className="todo-list-item">
+        const style = {
+            color: important ? 'tomato' : 'black',
+            fontWeight: important ? 'bold' : 'normal'
+        }
+
+        let nameOfClass = "todo-list-item";
+        const { done } = this.state;
+        if (done) {
+            nameOfClass += " done"
+        }
+
+        return (
+            <span className={nameOfClass}>
             <span className="todo-list-item-label"
                   style={style}
-            onClick={this.onClickLabel} >{label}</span>
+                  onClick={this.onClickLabel}>{label}</span>
             <button type={"button"}
                     className={"btn btn-outline-success btn-sm float-right"}>
-                <i className="fa fa-exclamation" /></button>
+                <i className="fa fa-exclamation"/></button>
             <button type="button"
                     className={"btn btn-outline-danger btn-sm float-right"}>
-                <i className="fa fa-trash-o" /></button>
+                <i className="fa fa-trash-o"/></button>
         </span>
-    )
- }
+        )
+    }
 };
